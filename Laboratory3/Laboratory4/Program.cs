4b97505fb3d108f96c745ff9da4d94e98ce5c1c2
Maxim Moskalyk <ictic.step@gmail.com>
2024-04-02 10:39:51 +0300

    Program with virtual methods implemented.

---
+++ Laboratory3/Laboratory4/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleTools;
+using Laboratory4.Core;
+
+namespace Laboratory4;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Triangle triangle = new(4.0, 60.0, 45.0);
+        ShowMenu(triangle);
+    }
+
+    private static void ShowMenu(Triangle triangle) =>
+        new ConsoleMenu()
+            .Add($"As {nameof(EquilateralTriangle)}", () => ShowEquilateralTriangleParameters(triangle))
+            .Add($"As {nameof(Triangle)}", () => ShowTriangleParameters(triangle))
+            .Add("Exit", ConsoleMenu.Close)
+            .Configure(config => config.Title = "Choose how to work with triangle.")
+            .Show();
+
+    private static void ShowEquilateralTriangleParameters(EquilateralTriangle triangle)
+    {
+        Console.WriteLine($"{triangle.GetType().Name}:");
+        Console.WriteLine($"Side length 1: {triangle.GetSide1()}");
+        Console.WriteLine($"Side length 2: {triangle.GetSide2()}");
+        Console.WriteLine($"Side length 3: {triangle.GetSide3()}");
+        Console.WriteLine($"Angle 1: {triangle.GetAngle1()}");
+        Console.WriteLine($"Angle 2: {triangle.GetAngle2()}");
+        Console.WriteLine($"Angle 3: {triangle.GetAngle3()}");
+        Console.WriteLine($"Perimeter: {triangle.GetPerimeter()}");
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+    
+    // ReSharper disable once SuggestBaseTypeForParameter
+    private static void ShowTriangleParameters(Triangle triangle)
+    {
+        Console.WriteLine($"{triangle.GetType().Name}:");
+        Console.WriteLine($"Side length 1: {triangle.GetSide1()}");
+        Console.WriteLine($"Side length 2: {triangle.GetSide2()}");
+        Console.WriteLine($"Side length 3: {triangle.GetSide3()}");
+        Console.WriteLine($"Angle 1: {triangle.GetAngle1()}");
+        Console.WriteLine($"Angle 2: {triangle.GetAngle2()}");
+        Console.WriteLine($"Angle 3: {triangle.GetAngle3()}");
+        Console.WriteLine($"Perimeter: {triangle.GetPerimeter()}");
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
